32bf421196724fff630efed36f8fd0e41e239a35
Shaikh Faisal Hossain <faisal.hossain.pk@gmail.com>
2019-09-22 03:26:40 +0600

    working with list

--- lib/main.dart
+++ lib/main.dart
@@ -13,16 +13,49 @@ class MyApp extends StatelessWidget{
     final wordpair=WordPair.random();
     return MaterialApp(
       title: 'First App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello'),
-        ),
-        body: Center(
-          child:Text(wordpair.asPascalCase)
-        ),
-      ),
+      home: RandomWords(),
     );
   }
 
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords>{
+  final _suggestions=<WordPair>[];
+  final _biggerFont=const TextStyle(fontSize: 18);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return Divider(); /*2*/
+
+          final index = i ~/ 2; /*3*/
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
 }
 
